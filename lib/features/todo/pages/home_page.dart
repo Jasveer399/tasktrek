d730acd7e4acbf92d619b7db34e2a4271871d87c
jassijasit <jasveersingh182002@gmail.com>
2024-03-22 14:02:21 +0530

    add Task Search functionality

--- lib/features/todo/pages/home_page.dart
+++ lib/features/todo/pages/home_page.dart
@@ -3,19 +3,22 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_vector_icons/flutter_vector_icons.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:tasktrek/common/helper/notification_helper.dart';
+import 'package:tasktrek/common/models/task_model.dart';
 import 'package:tasktrek/common/utils/constants.dart';
 import 'package:tasktrek/common/widgets/appstyle.dart';
 import 'package:tasktrek/common/widgets/custom_TextFild.dart';
 import 'package:tasktrek/common/widgets/reusable_text.dart';
 import 'package:tasktrek/features/todo/controllers/todo/todos_provider.dart';
 import 'package:tasktrek/features/todo/pages/addtask_page.dart';
+import 'package:tasktrek/features/todo/pages/update_task.dart';
 import 'package:tasktrek/features/todo/widgets/completedtask.dart';
 import 'package:tasktrek/features/todo/widgets/dayafterTMtask.dart';
 import 'package:tasktrek/features/todo/widgets/todo_task.dart';
+import 'package:tasktrek/features/todo/widgets/todo_tile.dart';
 import 'package:tasktrek/features/todo/widgets/tomrrow_task.dart';
 
 class Homepage extends ConsumerStatefulWidget {
-  const Homepage({super.key});
+  const Homepage({Key? key}) : super(key: key);
 
   @override
   ConsumerState<Homepage> createState() => _HomepageState();
@@ -28,6 +31,7 @@ class _HomepageState extends ConsumerState<Homepage>
   late NotificationHelper notifierHelper;
   late NotificationHelper notifiercontroller;
   TextEditingController search = TextEditingController();
+  List<Task> filtertask = [];
 
   @override
   void initState() {
@@ -38,9 +42,28 @@ class _HomepageState extends ConsumerState<Homepage>
     super.initState();
   }
 
+  void filterTasks(String query) {
+    if (query.isEmpty) {
+      setState(() {
+        filtertask.clear();
+      });
+      return;
+    }
+    final List<Task> filteredTasks = ref
+        .read(todosStateProvider)
+        .where((task) =>
+            task.title!.toLowerCase().contains(query.toLowerCase()) ||
+            task.desc!.toLowerCase().contains(query.toLowerCase()))
+        .toList();
+    setState(() {
+      filtertask = filteredTasks;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     ref.watch(todosStateProvider.notifier).refresh();
+    dynamic rcolor = ref.read(todosStateProvider.notifier).getRendomCl();
     return Scaffold(
       appBar: AppBar(
         elevation: 0,
@@ -56,20 +79,24 @@ class _HomepageState extends ConsumerState<Homepage>
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     ReusableText(
-                        text: "DashBoard",
-                        style: appstyle(18, AppConst.KLight, FontWeight.w500)),
+                      text: "DashBoard",
+                      style: appstyle(18, AppConst.KLight, FontWeight.w500),
+                    ),
                     Container(
                       height: 30.h,
                       width: 30.w,
                       decoration: BoxDecoration(
-                          color: AppConst.KLight,
-                          borderRadius: BorderRadius.circular(9)),
+                        color: AppConst.KLight,
+                        borderRadius: BorderRadius.circular(9),
+                      ),
                       child: GestureDetector(
                         onTap: () {
                           Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => AddTaskpage()));
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => AddTaskpage(),
+                            ),
+                          );
                         },
                         child: Icon(
                           Icons.add,
@@ -80,20 +107,18 @@ class _HomepageState extends ConsumerState<Homepage>
                   ],
                 ),
               ),
-              SizedBox(
-                height: 15.h,
-              ),
+              SizedBox(height: 15.h),
               CustomTextFiled(
                 controller: search,
                 hintText: " Search",
+                onChanged: (query) {
+                  filterTasks(query);
+                },
                 prefixIcon: Container(
                   padding: EdgeInsets.all(14.h),
-                  child: GestureDetector(
-                    onTap: () {},
-                    child: Icon(
-                      AntDesign.search1,
-                      color: AppConst.KGreyDk,
-                    ),
+                  child: Icon(
+                    AntDesign.search1,
+                    color: AppConst.KGreyDk,
                   ),
                 ),
                 suffixIcon: Icon(
@@ -101,44 +126,37 @@ class _HomepageState extends ConsumerState<Homepage>
                   color: AppConst.KGreyDk,
                 ),
               ),
-              SizedBox(
-                height: 10.h,
-              )
+              SizedBox(height: 10.h)
             ],
           ),
         ),
       ),
       body: SafeArea(
-          child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 20.w),
-        child: ListView(
-          children: [
-            SizedBox(
-              height: 40.h,
-            ),
-            Row(
-              children: [
-                Icon(
-                  FontAwesome.tasks,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20.w),
+          child: ListView(
+            children: [
+              SizedBox(height: 40.h),
+              Row(
+                children: [
+                  Icon(
+                    FontAwesome.tasks,
+                    color: AppConst.KLight,
+                  ),
+                  SizedBox(width: 10.w),
+                  ReusableText(
+                    text: "Today's Task",
+                    style: appstyle(18, AppConst.KLight, FontWeight.bold),
+                  ),
+                ],
+              ),
+              SizedBox(height: 15.h),
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20.r),
                   color: AppConst.KLight,
                 ),
-                SizedBox(
-                  width: 10.w,
-                ),
-                ReusableText(
-                    text: "Today's Task",
-                    style: appstyle(18, AppConst.KLight, FontWeight.bold))
-              ],
-            ),
-            SizedBox(
-              height: 15.h,
-            ),
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20.r),
-                color: AppConst.KLight,
-              ),
-              child: TabBar(
+                child: TabBar(
                   controller: tabController,
                   indicatorSize: TabBarIndicatorSize.label,
                   indicator: BoxDecoration(
@@ -157,8 +175,11 @@ class _HomepageState extends ConsumerState<Homepage>
                         child: Center(
                           child: ReusableText(
                             text: "Pending..",
-                            style:
-                                appstyle(16, AppConst.KGreyBk, FontWeight.bold),
+                            style: appstyle(
+                              16,
+                              AppConst.KGreyBk,
+                              FontWeight.bold,
+                            ),
                           ),
                         ),
                       ),
@@ -170,49 +191,102 @@ class _HomepageState extends ConsumerState<Homepage>
                         child: Center(
                           child: ReusableText(
                             text: "completed",
-                            style:
-                                appstyle(16, AppConst.KGreyBk, FontWeight.bold),
+                            style: appstyle(
+                              16,
+                              AppConst.KGreyBk,
+                              FontWeight.bold,
+                            ),
                           ),
                         ),
                       ),
                     ),
-                  ]),
-            ),
-            SizedBox(
-              height: 15.h,
-            ),
-            Container(
-              width: AppConst.kwidth,
-              height: AppConst.khight * 0.3,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(12.r),
-                child: TabBarView(
-                  controller: tabController,
-                  children: [
-                    Container(
-                        height: AppConst.khight * 0.3,
-                        color: AppConst.KGreyDk,
-                        child: TodayTask()),
-                    Container(
-                      height: AppConst.khight * 0.3,
-                      color: AppConst.KGreyDk,
-                      child: Completedtask(),
-                    ),
                   ],
                 ),
               ),
-            ),
-            SizedBox(
-              height: 15.h,
-            ),
-            TomorrowTask(),
-            SizedBox(
-              height: 10.h,
-            ),
-            DayAfterTM_Task(),
-          ],
+              SizedBox(height: 15.h),
+              Container(
+                width: AppConst.kwidth,
+                height: AppConst.khight * 0.3,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(12.r),
+                  child: TabBarView(
+                    controller: tabController,
+                    children: [
+                      Container(
+                        height: AppConst.khight * 0.3,
+                        color: AppConst.KGreyDk,
+                        child: filtertask.isEmpty
+                            ? TodayTask()
+                            : ListView.builder(
+                                itemCount: filtertask.length,
+                                itemBuilder: (context, index) {
+                                  final data = filtertask[index];
+                                  final isCompleted = ref
+                                      .read(todosStateProvider.notifier)
+                                      .getStatus(data);
+                                  return Todotile(
+                                    onTap: () {
+                                      ref
+                                          .read(todosStateProvider.notifier)
+                                          .deletetodo(data.id ?? 0);
+                                    },
+                                    editwidget: GestureDetector(
+                                        onTap: () {
+                                          titles = data.title.toString();
+                                          descs = data.desc.toString();
+                                          Navigator.push(
+                                              context,
+                                              MaterialPageRoute(
+                                                  builder: (context) =>
+                                                      UpdateTaskpage(
+                                                        id: data.id ?? 0,
+                                                      )));
+                                        },
+                                        child: Icon(
+                                          MaterialCommunityIcons
+                                              .circle_edit_outline,
+                                          color: Colors.white,
+                                        )),
+                                    color: rcolor,
+                                    tittext: data.title,
+                                    subtext: data.desc,
+                                    starttime: data.startTime,
+                                    endtime: data.endTime,
+                                    swicther: Switch(
+                                        value: isCompleted,
+                                        onChanged: (val) {
+                                          ref
+                                              .read(todosStateProvider.notifier)
+                                              .marktaskcomplet(
+                                                  data.id ?? 0,
+                                                  data.title.toString(),
+                                                  data.desc.toString(),
+                                                  1,
+                                                  data.date.toString(),
+                                                  data.startTime.toString(),
+                                                  data.endTime.toString());
+                                        }),
+                                  );
+                                },
+                              ),
+                      ),
+                      Container(
+                        height: AppConst.khight * 0.3,
+                        color: AppConst.KGreyDk,
+                        child: Completedtask(),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(height: 15.h),
+              TomorrowTask(),
+              SizedBox(height: 10.h),
+              DayAfterTM_Task(),
+            ],
+          ),
         ),
-      )),
+      ),
     );
   }
 }
